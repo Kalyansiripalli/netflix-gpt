1e464c88ce6d297606629669aa55b31f41447f9d
Kalyansiripalli <siripallivenkatakalyan@gmail.com>
2026-01-18 20:19:39 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world! how are you</h1>
+    </div>
+  );
+};
+
+export default App;
